355cdc0e5f6a490efc7de9f14c3bcda85d569251
georgemeng915 <georgemeng@gmail.com>
2026-07-27 16:55:22 -0500

    testing dep

--- src/test/java/com/example/orderprocessingproject/OrderProcesingProjectApplicationTests.java
+++ src/test/java/com/example/orderprocessingproject/OrderProcesingProjectApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.orderprocessingproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.kafka.bootstrap-servers=localhost:9092",
+        "spring.datasource.url=jdbc:postgresql://localhost:5432/orderdb",
+        "spring.datasource.username=postgres",
+        "spring.datasource.password=secretpassword"
+})
 class OrderProcesingProjectApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
